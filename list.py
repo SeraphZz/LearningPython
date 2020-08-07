0c5fd7afa8c480c20f878810b500d88a2685fc54
ZGZ <839958423@qq.com>
2020-08-07 13:31:08 +0800

    d

--- list.py
+++ list.py
@@ -1,3 +1,9 @@
-classmate = ['wy','dyz','zgz',2016]
+classmate = ['wy','dyz','zgz','mlt']
 print(classmate)
 a = len(classmate)
+classmate.append(601)
+print(classmate)
+classmate.append([83018,'forgot',83123,83161])
+print(classmate)
+classmate.insert(5,2016)
+print(classmate)
